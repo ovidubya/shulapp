4281659858f21f8a32d9dd8e2549cecf943c7d79
Shalom <Ovadia.Shalom@verint.com>
2018-08-21 18:03:39 -0500

    updated verbiage and seperated event types

--- resources/script.js
+++ resources/script.js
@@ -1,3 +1,6 @@
+function getEventType(object) {
+    return object.name.trim();
+}
 function appendEvent(shulname, object) {
     // <span class="badge badge-pill badge-danger">Cutting it close</span>
     // <p class="card black">
@@ -39,6 +42,26 @@ function convertTo24Hour(time) {
     return time.replace(/(am|pm)/, '');
 }
 
+function generateEventTypes(data) {
+    var list = document.querySelector('.list');
+    var result = [], i = 0;
+    for (var key in data) {
+        for(i = 0; i < data[key].length; i++) {
+            if(!result.includes(data[key][i].name.trim())) {
+                result.push(data[key][i].name.trim());
+            }
+        }
+    }
+    for(i = 0; i < result.length; i++) {
+        var title = document.createElement('div');
+            title.className = 'title';
+        var type = document.createElement('h2');
+            type.innerHTML = result[i];
+        title.appendChild(type);
+        list.appendChild(title);
+    }
+    // return result;
+}
 
 function getCurrentTime() {
     var currentDay = new Date();
@@ -63,9 +86,17 @@ function eventStatus(time) {
 
 function generateEventAndTimes(list) {
     var domLIST = document.querySelector('.list');
+    generateEventTypes(list);
     for (var key in list) {
         for(var i = 0; i < list[key].length; i++) {
-            domLIST.appendChild(appendEvent(key,list[key][i]));
+            // debugger;
+            var evtType = list[key][i].name.trim();
+            var listToInject = document.querySelectorAll('div.title');
+            listToInject = jQuery(listToInject).filter(function(index, element) {
+                return element.firstElementChild.innerHTML == evtType;
+            });
+            jQuery(listToInject).append(appendEvent(key,list[key][i]));
+            // domLIST.appendChild(appendEvent(key,list[key][i]));
         }
     }
 }
